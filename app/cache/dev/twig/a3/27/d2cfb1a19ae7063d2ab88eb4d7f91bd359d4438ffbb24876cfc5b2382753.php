c84a0f2cf36d1910d61d5e7eb8ce050785f46340
fraaag <khaled.elamri@esprit.tn>
2014-05-08 11:31:14 +0200

    07mai

--- app/cache/dev/twig/a3/27/d2cfb1a19ae7063d2ab88eb4d7f91bd359d4438ffbb24876cfc5b2382753.php
+++ app/cache/dev/twig/a3/27/d2cfb1a19ae7063d2ab88eb4d7f91bd359d4438ffbb24876cfc5b2382753.php
@@ -25,26 +25,30 @@ class __TwigTemplate_a327d2cfb1a19ae7063d2ab88eb4d7f91bd359d4438ffbb24876cfc5b23
         $this->parent->display($context, array_merge($this->blocks, $blocks));
     }
 
-    // line 3
+    // line 4
     public function block_nav1($context, array $blocks = array())
     {
-        // line 4
-        echo "                            <ul class=\"menu\">
-                                <li><a   href=\"";
         // line 5
+        echo "                             <ul class=\"menu\">
+                                <li><a  href=\"";
+        // line 6
         echo $this->env->getExtension('routing')->getPath("produit");
-        echo "\">Produit</a></li>
-                                    <li><a>Espace perso</a></li>
-                                   
+        echo "\">Produits</a></li>
+                                    <li><a href=\"";
+        // line 7
+        echo $this->env->getExtension('routing')->getPath("prestataire");
+        echo "\">Espace perso</a></li>
+                                    <li><a>Contact </a></li>
                             </ul>
-                              </ul>
+                            
+                              
   ";
     }
 
-    // line 12
+    // line 14
     public function block_ListePrest($context, array $blocks = array())
     {
-        // line 13
+        // line 15
         echo "     
                               
   ";
@@ -62,6 +66,6 @@ class __TwigTemplate_a327d2cfb1a19ae7063d2ab88eb4d7f91bd359d4438ffbb24876cfc5b23
 
     public function getDebugInfo()
     {
-        return array (  48 => 13,  45 => 12,  35 => 5,  32 => 4,  29 => 3,);
+        return array (  52 => 15,  49 => 14,  39 => 7,  35 => 6,  32 => 5,  29 => 4,);
     }
 }
